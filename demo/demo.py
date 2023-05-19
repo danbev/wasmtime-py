631290b4f5c0d1ab4739aaf25df59b6e91c7fc43
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-05-19 15:28:13 +0200

    Add runtime issue reproducer
    This commit contains a reproducer for an issue that we ran into when
    trying to extract types into a separate interface. The READMD.md
    contains details about the error and how to run the example to
    reproduce this issue.
    
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

---
+++ demo/demo.py
@@ -0,0 +1,14 @@
+from dist import Component, ComponentImports, imports
+from wasmtime import Store
+
+class Host(imports.Python):
+    def print(self, s: str):
+        print(s)
+
+def main():
+    store = Store()
+    demo = Component(store, ComponentImports(Host()))
+    demo.run(store)
+
+if __name__ == '__main__':
+    main()
